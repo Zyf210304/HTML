f143260cd7dc1cc78d267a67d30ca6adc0b5ebe6
张亚飞 <672233580@qq.com>
2019-12-22 23:15:18 +0800

    对象

--- JS/js/myjs.js
+++ JS/js/myjs.js
@@ -1,3 +1,16 @@
 function outJSF() {
 	alert("外部声明");
 }
+
+function nextLine(){
+	document.write("<br \>");
+}
+
+function docWrite(a) {
+	document.write(a);
+}
+
+function docWriteLn(a) {
+	nextLine();
+	document.write(a);
+}
